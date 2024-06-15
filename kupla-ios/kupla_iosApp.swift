abcc9834b0254282843805ca19b7c8219928fcab
Eemeli Mark <eemelijoonatan@gmail.com>
2024-06-15 20:15:16 +0300

    Initial Commit

---
+++ kupla-ios/kupla_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kupla_iosApp.swift
+//  kupla-ios
+//
+//  Created by Eemeli Mark on 15.6.2024.
+//
+
+import SwiftUI
+
+@main
+struct kupla_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
